1cb2791c5e4c3db64b6ec37d96cfb202af83d1c8
takashi247 <takashi247@gmail.com>
2023-03-12 11:05:27 +0900

    modify: indent using space

--- ft_strlen.s
+++ ft_strlen.s
@@ -1,14 +1,14 @@
-section	.text
-	global _ft_strlen
+section .text
+  global _ft_strlen
 
 _ft_strlen:
-	mov	rdx, 0
-	jmp	.loop
+  mov	rdx, 0
+  mp	.loop
 .loop:
-	cmp	BYTE[rdi + rdx], 0
-	je	.exit
-	inc	rdx
-	jmp	.loop
+  cmp	BYTE[rdi + rdx], 0
+  je	.exit
+  inc	rdx
+  jmp	.loop
 .exit:
-	mov	rax, rdx
-	ret
+  mov	rax, rdx
+  ret
