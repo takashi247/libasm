0c0b3a2e94da11b6ad5007f81f1f52215bf7db6e
takashi247 <takashi247@gmail.com>
2023-03-13 19:14:30 +0900

    add: comments in ft_write

--- ft_write.s
+++ ft_write.s
@@ -9,12 +9,12 @@ section .text
 _ft_write:
   mov rax, 0x2000004; syscall number for write in MacOS
   syscall
-  jc error
+  jc error; in MacOS, carry flag will be set when error occurs in syscall and error number will be set in rax
   ret
 
 error:
-  mov rdi, rax
-  call ___error
-  mov [rax], rdi
-  mov rax, -1
+  mov rdi, rax; temporarily store errno stored in rax in rdi
+  call ___error; helper function to get address of errno
+  mov [rax], rdi; set errno in the address obtained from ___error
+  mov rax, -1; set returned value
   ret
